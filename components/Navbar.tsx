040a3da51394f8e499b2197689af36ef89c1ab8a
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2024-06-27 11:21:26 +0530

    feat: add aceternityUi ,Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+
+  function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null);
+    return (
+      <div
+        className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      >
+        <Menu setActive={setActive}>
+            <HoveredLink href="/">
+            <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+            </HoveredLink>
+            <HoveredLink href="/service">
+            <MenuItem setActive={setActive} active={active} item="Service">
+            <div className="flex flex-col text-sm space-y-4">
+            <HoveredLink href="#">
+            All Services
+            </HoveredLink>
+            <HoveredLink href="#">
+            Basic
+            </HoveredLink>
+            <HoveredLink href="#">
+            All cources
+            </HoveredLink>
+            <HoveredLink href="#">
+            All cources
+            </HoveredLink>
+            <HoveredLink href="#">
+            All cources
+            </HoveredLink>
+            </div>
+            </MenuItem>
+            </HoveredLink>
+            <HoveredLink href="/contact">
+            <MenuItem setActive={setActive} active={active} item="Contact"></MenuItem>
+            </HoveredLink>
+
+        </Menu>
+      </div>
+    )
+}
+export default Navbar;
